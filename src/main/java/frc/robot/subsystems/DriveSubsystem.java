fb2dbd6e64c9a293e9235cd83667c1f098bca45e
OddDuck <alexh@ieee.org>
2023-11-14 14:52:44 -0600

    Update to Swivels

---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,164 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.SwerveConstants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveSubsystem extends SubsystemBase {
+  // Robot swerve modules
+  private final SwerveModuleOffboard m_frontLeft =
+    new SwerveModuleOffboard(
+      SwerveConstants.kFrontLeftDriveMotorPort,
+      SwerveConstants.kFrontLeftTurningMotorPort,
+      SwerveConstants.kFrontLeftMagEncoderPort,
+      SwerveConstants.kFrontLeftMagEncoderOffsetDegrees);
+
+  private final SwerveModuleOffboard m_rearLeft =
+    new SwerveModuleOffboard(
+      SwerveConstants.kRearLeftDriveMotorPort,
+      SwerveConstants.kRearLeftTurningMotorPort,
+      SwerveConstants.kRearLeftMagEncoderPort,
+      SwerveConstants.kRearLeftMagEncoderOffsetDegrees);
+
+  private final SwerveModuleOffboard m_frontRight =
+    new SwerveModuleOffboard(
+      SwerveConstants.kFrontRightDriveMotorPort,
+      SwerveConstants.kFrontRightTurningMotorPort,
+      SwerveConstants.kFrontRightMagEncoderPort,
+      SwerveConstants.kFrontRightMagEncoderOffsetDegrees);
+
+  private final SwerveModuleOffboard m_rearRight =
+    new SwerveModuleOffboard(
+      SwerveConstants.kRearRightDriveMotorPort,
+      SwerveConstants.kRearRightTurningMotorPort,
+      SwerveConstants.kRearRightMagEncoderPort,
+      SwerveConstants.kRearRightMagEncoderOffsetDegrees);
+
+  // The imu sensor
+  private final PigeonIMU m_imu = new PigeonIMU(SwerveConstants.kIMU_ID);
+  
+  // Odometry class for tracking robot pose
+  SwerveDriveOdometry m_odometry =
+      new SwerveDriveOdometry(
+          DriveConstants.kDriveKinematics,
+          Rotation2d.fromDegrees(m_imu.getYaw()),
+          new SwerveModulePosition[] {
+            m_frontLeft.getPosition(),
+            m_frontRight.getPosition(),
+            m_rearLeft.getPosition(),
+            m_rearRight.getPosition()
+          });
+
+  /** Creates a new DriveSubsystem. */
+  public DriveSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // Update the odometry in the periodic block
+    m_odometry.update(
+        Rotation2d.fromDegrees(m_imu.getYaw()),
+        new SwerveModulePosition[] {
+          m_frontLeft.getPosition(),
+          m_frontRight.getPosition(),
+          m_rearLeft.getPosition(),
+          m_rearRight.getPosition()
+        });
+  }
+
+  /**
+   * Returns the currently-estimated pose of the robot.
+   *
+   * @return The pose.
+   */
+  public Pose2d getPose() {
+    return m_odometry.getPoseMeters();
+  }
+
+  /**
+   * Resets the odometry to the specified pose.
+   *
+   * @param pose The pose to which to set the odometry.
+   */
+  public void resetOdometry(Pose2d pose) {
+    m_odometry.resetPosition(
+        Rotation2d.fromDegrees(m_imu.getYaw()),
+        new SwerveModulePosition[] {
+          m_frontLeft.getPosition(),
+          m_frontRight.getPosition(),
+          m_rearLeft.getPosition(),
+          m_rearRight.getPosition()
+        },
+        pose);
+  }
+
+  /**
+   * Method to drive the robot using joystick info.
+   *
+   * @param xSpeed Speed of the robot in the x direction (forward).
+   * @param ySpeed Speed of the robot in the y direction (sideways).
+   * @param rot Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
+   */
+  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+    var swerveModuleStates =
+        DriveConstants.kDriveKinematics.toSwerveModuleStates(
+            fieldRelative
+                ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, Rotation2d.fromDegrees(m_imu.getYaw()))
+                : new ChassisSpeeds(xSpeed, ySpeed, rot));
+    SwerveDriveKinematics.desaturateWheelSpeeds(
+        swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond);
+    m_frontLeft.setDesiredState(swerveModuleStates[SwerveConstants.kSwerveFL_enum]);
+    m_frontRight.setDesiredState(swerveModuleStates[SwerveConstants.kSwerveFR_enum]);
+    m_rearLeft.setDesiredState(swerveModuleStates[SwerveConstants.kSwerveRL_enum]);
+    m_rearRight.setDesiredState(swerveModuleStates[SwerveConstants.kSwerveRR_enum]);
+  }
+
+  /**
+   * Sets the swerve ModuleStates.
+   *
+   * @param desiredStates The desired SwerveModule states.
+   */
+  public void setModuleStates(SwerveModuleState[] desiredStates) {
+    SwerveDriveKinematics.desaturateWheelSpeeds(
+        desiredStates, DriveConstants.kMaxSpeedMetersPerSecond);
+    m_frontLeft.setDesiredState(desiredStates[SwerveConstants.kSwerveFL_enum]);
+    m_frontRight.setDesiredState(desiredStates[SwerveConstants.kSwerveFR_enum]);
+    m_rearLeft.setDesiredState(desiredStates[SwerveConstants.kSwerveRL_enum]);
+    m_rearRight.setDesiredState(desiredStates[SwerveConstants.kSwerveRR_enum]);
+  }
+
+  /** Resets the drive encoders to currently read a position of 0. */
+  public void resetEncoders() {
+    m_frontLeft.resetEncoders();
+    m_rearLeft.resetEncoders();
+    m_frontRight.resetEncoders();
+    m_rearRight.resetEncoders();
+  }
+
+  /** Zeroes the heading of the robot. */
+  public void zeroHeading() {
+    m_imu.setYaw(0);
+  }
+
+  /**
+   * Returns the heading of the robot.
+   *
+   * @return the robot's heading in degrees, from -180 to 180
+   */
+  public double getHeading() {
+    return m_imu.getYaw();
+  }
+}
